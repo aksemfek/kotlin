8c2266f8e48db5eace9ab810111902d7a1845669
jeon <wjadbwjd51@gamail.com>
2024-04-18 12:52:29 +0900

    feat: also 함수

---
+++ kotlin_ex/src/main/kotlin/ex11/Also.kt
@@ -0,0 +1,20 @@
+package org.example.ex11
+
+fun main(){
+    // also / 또한
+    // let 과 달리 내부에서만 작동
+
+    var userDto = UserDto(
+        name = "홍길동"
+    ).also{dto: UserDto->
+        println(dto)
+
+//        UserDto(
+//            "유관순"
+//        )
+        dto.name = "유관순"
+    }
+
+    println("result $userDto")
+
+}
